cfdef540aeea52315e08fb4862bad815afccd970
Martin Lowe <martinlowe673@gmail.com>
2024-08-21 08:15:38 +0100

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,23 @@
-import "@/styles/globals.css"
+import "./globals.css"
 import { Inter as FontSans } from "next/font/google"
 
 import { cn } from "@/lib/utils"
+import { Metadata } from "next"
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
+export const metadata: Metadata = {
+  title: "Home",
+  description: "your go to live editor"
+}
+
 export default function RootLayout({ children }: {children:React.ReactNode}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+  
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
