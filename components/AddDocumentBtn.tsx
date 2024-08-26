d526a29240c7ab6210305b4c407a18a449fc6f5e
Martin Lowe <martinlowe673@gmail.com>
2024-08-26 13:34:56 +0100

    document handler

--- components/AddDocumentBtn.tsx
+++ components/AddDocumentBtn.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { Button } from './ui/button'
 
 const AddDocumentBtn = ({userId, email }:AddDocumentBtnProps) => {
+    const addDocumentHandler = async () = >{
+
+    }
   return (
-  <Button>
+  <Button type="submit" onClick={addDocumentHandler} className='gradient-blue flex gap-1 shadow-md'>
     <p className='hidden sm:block'>Start a blank document</p>
   </Button>
   )
