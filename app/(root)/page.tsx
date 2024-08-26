ad7c0b1e55060ba4ee96c1bad6c814f0560c1e8b
Martin Lowe <martinlowe673@gmail.com>
2024-08-26 13:16:31 +0100

    classname on header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,15 @@
+import Header from '@/components/Header'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
+  <main className='home-container'>
+    <Header className='sticky left'>
+
+    </Header>
+
+  </main>
   )
 }
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,9 @@ import Image from 'next/image'
 
 
 
-const Header = ({children}:HeaderProps) => {
+const Header = ({children,className}:HeaderProps) => {
   return (
-    <div className='header'>
+    <div className={cn("header", className)}>
         <Link href="/" className='md:flex-1'>
         <Image src="/assets/icons/logo.svg" alt="logo with name" width={120} height={32} className='hidden md:block'/>
         <Image src="/assets/icons/logo-icon.svg" alt="logo" width={32} height={32} className='mr-2 md:hidden'/>
