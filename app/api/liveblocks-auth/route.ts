ab71e4fcaae38d0b5bc1b892d122b5ca47e80f02
Martin Lowe <martinlowe673@gmail.com>
2024-08-26 12:16:03 +0100

    email address

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -1,25 +1,32 @@
 import { liveblocks } from "@/lib/liveblocks";
-import { currentUser } from "@clerk/nextjs/server";
+import { EmailAddress, currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { emit } from "process";
 
 
 
 
 
 export async function POST(request: Request) {
+
+
     const clerkUser = await currentUser();
     if (!clerkUser) redirect('/sign-in')
+
+    const {id, firstName, lastName,imageUrl,emailAddresses} = clerkUser
   // Get the current user from your database
-  const user = __getUserFromDB__(request);
+  
 
   // Identify the user and return the result
-  const { status, body } = await liveblocks.identifyUser(
-    {
-      userId: user.id,
-      groupIds, // Optional
-    },
-    { userInfo: user.metadata },
-  );
+    const user = {
+        id: clerkUser.id,
+        info:{
+            id,
+            name:`${firstName} ${lastName}`,
+            email: emailAddress[0].emailAddress,
+
+        }
+    }
 
   return new Response(body, { status });
 }
