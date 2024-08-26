14c1496ce342bd4150f88a20f6596b5ff797607f
Martin Lowe <martinlowe673@gmail.com>
2024-08-26 13:06:02 +0100

    room actions

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -1,7 +1,33 @@
 import {nanoid} from "nanoid"
+import { liveblocks } from "../liveblocks";
 
 export const createDocument = async({userId, email}: CreateDocumentParams) =>{
 
-    
-    const roomId = 
+
+    const roomId = nanoid()
+
+    try{
+        const metadata = {
+            creatorId:userId,
+            email,
+            title:'Untitled'
+        }
+
+        const usersAccesses: RoomAccesses = {
+            [email]:['room:write']
+        }
+
+        const room = await liveblocks.createRoom("my-room-id", {
+            defaultAccesses: ["room:read", "room:presence:write"],
+            groupsAccesses: {
+              "my-group-id": ["room:write"],
+            },
+            usersAccesses: {
+              "my-user-id": ["room:write"],
+            },
+          });
+
+    } catch(error){
+        console.log(`Error happened while creating a room: ${error}`)
+    }
 }
