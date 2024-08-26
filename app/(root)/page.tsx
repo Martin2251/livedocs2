ae98ff70f85c4e161e1604fd16ba711bf29cf830
Martin Lowe <martinlowe673@gmail.com>
2024-08-26 14:19:44 +0100

    styles issyes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,15 +14,16 @@ const Home = async() => {
 
   const documents =[]
   return (
-  <main className='home-container'>
-    <Header className='sticky left-0 top-0'>
-    <div className='flex items-center gap-2 lg:gap-4'>
-      Notification
-      <SignedIn>
-        <UserButton  />
-      </SignedIn>
-    </div>
+    <main className="home-container">
+    <Header className="sticky left-0 top-0">
+      <div className="flex items-center gap-2 lg:gap-4">
+      
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </Header>
+ 
     {documents.length > 0 ? (
       <div>
 
